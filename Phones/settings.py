1aab9495150755240d0f4bfc949068cf0d38a726
Ashish <shirsalkarashishprakash@gmail.com>
2022-01-04 16:32:55 +0530

    try for postsql v3

--- Phones/settings.py
+++ Phones/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-up$$hj+))_)8^x$a7gh_tk&u80^%)6k*(2r-c7wisz3cojm1td'
-#SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = 'django-insecure-up$$hj+))_)8^x$a7gh_tk&u80^%)6k*(2r-c7wisz3cojm1td'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ["mobileadvancefilter.herokuapp.com/","localhost"]
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["mobileadvancefilter.herokuapp.com/","localhost"]
+#ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
